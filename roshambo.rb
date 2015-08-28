5689dd52bfa61de07d40538d02c21e819a9809fd
Amanda Porto <amandaporto@msn.com>
2015-08-28 11:42:58 -0400

    Quit if no name

--- roshambo.rb
+++ roshambo.rb
@@ -1,6 +1,21 @@
 
 def match
   puts "Player's name:"
-  name = gets.chomp
+  name = gets.chomp.upcase
+  @name = name
+  if name.empty?
+    puts "No name, no game!"
+    exit
+  end
 end
 match
+
+
+
+
+
+
+# IDEAS
+# Design around both choices???
+#
+#
