86ae71610b930ac40ba3a88e5f32a2be0ff8d996
Amanda Porto <amandaporto@msn.com>
2015-08-29 13:48:12 -0400

    Match, Bout, Engagement logic coded but not working

--- roshambo.rb
+++ roshambo.rb
@@ -1,3 +1,19 @@
+e_win = e_loss = e_draw = 0
+b_win = b_loss = b_draw = 0
+m_win = m_loss = m_draw = 0
+
+@e_win = e_win
+@e_loss = e_loss
+@e_draw = e_draw
+
+@b_win = b_win
+@b_loss = b_loss
+@b_draw = b_draw
+
+@m_win = b_win
+@m_loss = b_loss
+@m_draw = b_draw
+
 
 def get_name
   puts "Player's name:"
@@ -8,20 +24,57 @@ def get_name
     exit
   end
 end
-get_name
 
 def engagement
-  puts "Choose rock, paper, or scissors."
-  user_choice = gets.chomp.capitalize
-  puts "#{@name}'S CHOICE: #{user_choice}"
-  comp_choice = ["Rock", "Paper", "Scissors"].sample
-  puts "COMPUTER CHOICE: #{comp_choice}"
+  until @m_win == 1 || @m_loss == 1 || @m_win == 1
+    until @b_win == 2 || @b_loss == 2
+      until @e_win == 2 || @e_loss == 2
+        comp_choice = ["Rock", "Paper", "Scissors"].sample
+        puts comp_choice
+        puts "Choose rock, paper, or scissors."
+        user_choice = gets.chomp.capitalize
+        puts "~~~~~~~~~~~~~~GAME~~~~~~~~~~~~~~"
+        puts "   #{@name}    VS    COMPUTER \n   #{user_choice}            #{comp_choice}\n\n"
+          if user_choice == comp_choice
+            puts "    >>>>>ENGAGEMENT DRAW<<<<<"
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            @e_draw += 1
+          elsif
+            (user_choice == "Rock" && comp_choice == "Scissors" ||
+            user_choice == "Paper" && comp_choice == "Rock" ||
+            user_choice == "Scissors" && comp_choice == "Paper")
+            puts "    >>>>>ENGAGEMENT WON!<<<<<"
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            @e_win += 1
+          else
+            puts "    >>>>>ENGAGEMENT LOST<<<<<"
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            @e_loss += 1
+          end
+        end
+        if @e_win == 2
+          puts "BOUT WON!!"
+          @b_win += 1
+        elsif @e_loss == 2
+          puts "BOUT LOST"
+          @b_loss += 1
+        elsif @e_draw == 2
+          puts "BOUT DRAW"
+          @b_draw += 1
+        end
+      end
+    if @b_win == 2
+      puts "MATCH WON"
+      @m_win += 1
+    elsif @b_loss == 2
+      puts "MATCH LOST"
+      @m_win += 1
+    elsif @b_draw == 2
+      puts "MATCH DRAW"
+      @m_win += 1
+    end
+  end
 end
 
-
-
-
-# IDEAS
-# Design around both choices???
-#
-#
+get_name
+engagement
